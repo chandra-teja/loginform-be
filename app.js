923a8220116e4c912381a0d961fb9361c57bb883
CHANDRA TEJA <chandra.teja@licious.com>
2025-02-17 17:35:48 +0530

    Enable cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 8080;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-
+app.use(cors());
 app.get("/", (req, res) => {
   console.log("get api called");
   res.send("get api called");
